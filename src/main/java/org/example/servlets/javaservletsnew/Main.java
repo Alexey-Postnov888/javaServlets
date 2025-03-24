0263ba3f8f39a5bf28187e54fb57d45804a3066b
Alexey Postnov <adpostnov@yandex.ru>
2025-03-24 17:42:20 +0500

    fixes (+ jetty)

---
+++ src/main/java/org/example/servlets/javaservletsnew/Main.java
@@ -0,0 +1,14 @@
+package org.example.servlets.javaservletsnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
